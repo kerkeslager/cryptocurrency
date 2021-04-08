d47183928244329fd6d895957fd0f5126619e2cf
David Kerkeslager <kerkeslager@gmail.com>
2021-04-07 20:59:45 -0400

    Get peers working

--- peer.py
+++ peer.py
@@ -1,39 +1,94 @@
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--peer', type=str, action='append', dest='peers', metavar='PEER', help='initial peers to connect to')
-    parser.add_argument('--port', type=int, default=5555, help='port to listen on')
+    parser.add_argument(
+        '-p',
+        '--peer',
+        action='append',
+        default=[],
+        dest='peers',
+        help='initial peers to connect to',
+        metavar='PEER',
+        type=str,
+    )
+    parser.add_argument(
+        '--port',
+        default=5555,
+        help='port to listen on',
+        type=int,
+    )
     args = parser.parse_args()
 
-    import selectors
-    import socket
+    import asyncio
+    import json
 
-    sel = selectors.DefaultSelector()
+    peers = []
 
-    def accept(sock, mask):
-        conn, addr = sock.accept()  # Should be ready
-        print('accepted', conn, 'from', addr)
-        conn.setblocking(False)
-        sel.register(conn, selectors.EVENT_READ, read)
+    class DiscoveryProtocol(asyncio.DatagramProtocol):
+        def connection_made(self, transport):
+            self.transport = transport
 
-    def read(conn, mask):
-        data = conn.recv(1024)  # Should be ready
-        if data:
-            print('echoing', repr(data), 'to', conn)
-            conn.send(data)  # Hope it won't block
-        else:
-            print('closing', conn)
-            sel.unregister(conn)
-            conn.close()
+            for peer in args.peers:
+                peers.append(('127.0.0.1', int(peer)))
 
-    sock = socket.socket()
-    sock.bind(('localhost', args.port))
-    sock.listen(100)
-    sock.setblocking(False)
-    sel.register(sock, selectors.EVENT_READ, accept)
+            add_peer_msg = json.dumps({'action': 'add-peer'}).encode('utf-8')
+            request_peer_msg = json.dumps({'action': 'request-peers'}).encode('utf-8')
 
-    while True:
-        events = sel.select()
-        for key, mask in events:
-            callback = key.data
-            callback(key.fileobj, mask)
+            for peer in peers:
+                self.transport.sendto(add_peer_msg, peer)
+                self.transport.sendto(request_peer_msg, peer)
+
+        def datagram_received(self, data, addr):
+            try:
+                data = json.loads(data)
+            except:
+                print('Invalid message received from {}'.format(addr))
+                return
+
+            if not isinstance(data, dict):
+                print('Invalid message received from {}'.format(addr))
+                return
+
+            action = data.get('action')
+
+            if not action:
+                print('Invalid message received from {}'.format(addr))
+                return
+
+            if action == 'add-peer':
+                peers.append(addr)
+
+            elif action == 'request-peers':
+                print('request-peers')
+                self.transport.sendto(
+                    json.dumps({
+                        'action': 'respond-peers',
+                        'peers': ['{}:{}'.format(*peer) for peer in peers],
+                    }).encode('utf-8'),
+                    addr,
+                )
+
+            elif action == 'respond-peers':
+                print('respond-peers')
+                print(data)
+                for p in data.get('peers',[]):
+                    phost, pport = p.split(':')
+                    pport = int(pport)
+                    peer = (phost, pport)
+                    if not peer in peers:
+                        peers.append(peer)
+                        print('Added peer {}'.format(peer))
+                        add_peer_msg = json.dumps({'action': 'add-peer'}).encode('utf-8')
+                        self.transport.sendto(add_peer_msg, peer)
+
+            else:
+                print('Unknown action {}'.format(action))
+
+    loop = asyncio.get_event_loop()
+    t = loop.create_datagram_endpoint(
+        DiscoveryProtocol,
+        local_addr=('0.0.0.0', args.port),
+    )
+    loop.run_until_complete(t)
+    print('Running on port {}'.format(args.port))
+    loop.run_forever()
